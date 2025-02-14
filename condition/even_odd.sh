299fa839402310c50e13a4f76600b3f4f4eab96a
Pajaykumar97 <iamajaykumar.p97@gmail.com>
2025-02-14 19:03:34 +0530

    checking even or odd

---
+++ condition/even_odd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Check if a Number is an Even or Odd.....
+
+echo "Enter the number :"
+read num
+
+if [ $num / 2 == 0 ]; then
+    echo "Given number is even"
+elif [ $num -ne 0 ]; then
+    echo "please enter correct number"
+else 
+    echo "given number is odd"
+fi
