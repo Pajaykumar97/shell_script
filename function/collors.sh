09701f832d2495a53c4f017ec87f7b37c2296aff
Pajaykumar97 <iamajaykumar.p97@gmail.com>
2025-02-14 19:52:58 +0530

    using fuction installing packages

---
+++ function/collors.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+check_root () {
+    if [ $USERID -ne 0 ];then
+        echo -e " $R u must have the root access to execute this $N"
+        exit 1
+    fi 
+}
+
+check_root
+
+validate () {
+    if [ $1 -ne 0 ]; then
+        echo -e "$1 $R installig failure $N"
+        exit 1
+    else
+        echo -e "$1 $G installig success $N"
+    fi
+}
+
+dnf list installed mysql
+if [ $? -ne 0 ]; then
+    dnf install mysql -y
+    if [ $? -ne 0 ];then
+    validate $1 "installing mysql"
+else 
+    echo -e " $1 $Y installed already $N"
+fi 
