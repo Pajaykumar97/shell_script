839145b725add052cc950d9fadafb7e04871fe55
Pajaykumar97 <iamajaykumar.p97@gmail.com>
2025-02-14 18:50:51 +0530

    checking java installed or not

---
+++ installing/checking_java.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+    echo "you must have the root access to execute this"
+    exit 1
+fi
+
+dnf list installed java -y
+if [ $? -ne 0 ]; then
+    dnf install java -y
+    if [ $? -ne 0 ]; then
+        echo "installing jave failure"
+        exit 1
+    else 
+        echo "installing java success"
+    fi
+else 
+    echo "already installed java"
+fi 
