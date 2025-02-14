69e437be2b4f812cf6f093ad3a2f661722419667
Pajaykumar97 <iamajaykumar.p97@gmail.com>
2025-02-14 18:25:32 +0530

    user

--- arrays/loop_arr.sh
+++ arrays/loop_arr.sh
@@ -4,12 +4,13 @@ Fruits=("Apple" "Grape" "Orange" "watermelon")
 for item in "${Fruits[@]}"; do
 echo $item
 done
-
+echo "============================="
 
 fruits=("Orange" "green apple" "Guvva")
 for item in "${fruits[@]}"; do
 echo $item
 done
+echo "==========================="
 
 vihicle=("car" "bus" "cycle")
 for item in "${vihicle[@]}"; do
