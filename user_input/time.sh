34a9bd7a724ec6bddc3fd32e96291e6200b20dad
Pajaykumar97 <iamajaykumar.p97@gmail.com>
2025-02-14 16:38:33 +0530

    user

--- user_input/time.sh
+++ user_input/time.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-read -p "Enter user_Name"
-read -s -t 5 -p "Enter your password"
+read -p "Enter user_Name :"
+read -s -t 5 -p "Enter your password :"
+
+echo "U r not entered your password wsith in 5 sec's"
